5a1465d083f1761e99006657bb90f5dafe3576ae
seho-san <sunnyseho01@gmail.com>
2025-03-17 00:35:32 +0900

    초기 커밋.

---
+++ Divisor, Multiple, prime number/n2501.cpp
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main() {
+//    int n, k;
+//    int count = 0; //약수의 갯수
+//    int i; //divisor
+//    scanf("%d %d", &n, &k);
+//
+//    for (i = 1; i <= n; i++) {
+//        if (n % i == 0)
+//            count++;
+//        if (count == k)
+//            break;
+//    }
+//    if (i <= n) 
+//        printf("%d", i);
+//    else 
+//        printf("0");
+//}
+//
---
+++ Divisor, Multiple, prime number/n5086.cpp
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//int main() {
+//	int a, b;
+//	while (1) {
+//		scanf("%d %d", &a, &b);
+//		if (a == 0 && b == 0) break;
+//		if(b%a==0)
+//			printf("factor\n");
+//		else if (a % b == 0) 
+//			printf("multiple\n");
+//		else	
+//			printf("neither\n");
+//	}
+//}
